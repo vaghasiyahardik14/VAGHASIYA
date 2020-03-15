fb83c4391c7eb512c91706d304e5baf306514327
vaghasiyahardik14 <62205457+vaghasiyahardik14@users.noreply.github.com>
2020-03-15 17:36:55 +0530

    itteration

---
+++ iteaertion.py
@@ -0,0 +1,21 @@
+#list is made
+
+lst1 = ["mumbai","pune","delhi","patna"]
+
+for city in lst1:
+
+    print(city)
+
+    print("\n")
+
+    lst2 = ["python","perl","c","c++","java"]
+
+    for lng in lst2:
+
+        print(lng)
+
+        print("\n")
+
+        for s in "Iteration is easy":
+
+            print(s, end="")
